039f99f602013a428404529411e1065c254a4d5f
Jineusraja <jineus2001raja@gamil.com>
2024-03-18 19:01:53 +0530

    First commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const deliveryController = require('../controllers/deliveryController');
 
-router.post('/calculate-delivery', deliveryController.calculateDeliveryCost);
+router.post('/calculate', deliveryController.calculateDeliveryCost);
 
 module.exports = router;
